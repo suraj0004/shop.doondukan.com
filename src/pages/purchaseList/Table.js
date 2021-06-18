2999f52527505ece360d9cc77ddd39ad591478b4
suraj0004 <surajupadhyaa@gmail.com>
2021-06-19 01:30:02 +0530

    update: pagination and filter for purchase product list

--- src/pages/purchaseList/Table.js
+++ src/pages/purchaseList/Table.js
@@ -1,11 +1,61 @@
 import React from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
+import NoRecord from '../../components/NoRecord'
 
 function Table(props) {
 
-  const {main,temp} = props.data;
+  const {data, currentPage, perPage} = props;
   let sno = 0;
+
+  return (
+    <div className="row">
+      <NoRecord data_count={data.length} />
+      {data.map((item, index) => {
+        
+        return (
+          <div className="col-12 col-md-6 p-3 " key={index.toString()}>
+            <div className="card shadow">
+              <div className="card-header">
+                {" "}
+                <strong> #{ ( (currentPage - 1) * perPage ) +  (index + 1)}  </strong>
+              </div>
+              <div className="card-body row">
+                <div className="col-sm-4 text-center img-responsive">
+                  <img
+                    src={item.product.image}
+                    className="img-fluid"
+                    alt={item.product.name}
+                  />
+                </div>
+                <div className="col-sm-8 text-left">
+                  <p>
+                    <strong>Product: </strong>
+                    {item.product.name +
+                      " | " +
+                      item.product.weight +
+                      item.product.weight_type}{" "}
+                  </p>
+                  <p> <strong>Purchased Quantity: </strong> {item.quantity} </p>
+                  <p>
+                  <strong>Purchased Date: </strong>
+                    <Moment
+                      local
+                      format="D MMM, YYYY"
+                      date={item.created_at}
+                    />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      {/* </tbody> */}
+      {/* </table> */}
+    </div>
+  );
+
     return (
         <div className="card">
            
@@ -27,36 +77,9 @@ function Table(props) {
             <tbody>
 
 
-
-              
-            {
-                   temp.map( (item) => {
-                      sno++;
-                      console.log(sno);
-                    return (
-                      
-                       <tr key={sno.toString() } >
-                       <td> {sno } <span className="badge badge-warning">Custom Product</span> </td>
-                       <td> <Moment 
-                                
-                                //  parse="YYYY-MM-DDTHH:mm"
-                                 local
-                                 format="D MMM, YYYY h:mm a"
-                                 date={ item.created_at }
-                                 />
-                            </td>
-                       <td> { item.temp_product.name + " | " + item.temp_product.weight + item.temp_product.weight_type}  </td>
-                       <td> {item.quantity} </td>
-                       <td> {item.price} </td>
-                       <td>{item.quantity * item.price } </td>
-                     </tr>
-                    );
-                   })
-                }
-
                 
                 {
-                    main.map( (item) => {
+                    data.map( (item) => {
                       sno++;
                       console.log(sno);
                      return (
