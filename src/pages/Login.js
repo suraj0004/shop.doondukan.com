ce355ee7ad3adff10bf78edd55bd4290c74af817
VipinFarswan <vipinfarswan421@gmail.com>
2021-06-23 23:14:39 +0530

    fix warning issue

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
 import axios from "axios";
 import UrlService from "../services/UrlService";
-import  { Redirect } from 'react-router-dom'
 const Loader = () => {
   return <ClipLoader loading color="#0069d9" />;
 };
